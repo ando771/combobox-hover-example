8c0b72ac09eeae34a53d885d9bea5cb9125a05dd
ando771 <denis_krivosheev@bk.ru>
2016-11-01 13:00:24 +0500

    Change example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,19 +33,30 @@ function foundItem(item) {
   return <span>{item}</span>;
 }
 
-const App = () => <div>
-  <div className={styles.block}>
-    <h3>Наведи чтобы выбрать значение</h3>
-    <div className={styles.hovered}>
-      <ComboBox
-        source={getSource}
-        renderItem={foundItem}
-        placeholder="Выберите значение"
-        openButton
-      />
+export default class App extends React.Component {
+  state = {
+    value: ''
+  }
+
+  render(){
+    return <div className={styles.main}>
+      <h3>Пример:</h3>
+      <p>Combobox появляется только при hover. Но когда курсор находится над меню Combobox`а, input пропадает.</p>
+      <p>Также, при прокрутке меню, оно съезжает в левый верхний угол страницы.</p>
+      <div className={styles.block}>
+        <span className={styles.text}>{this.state.value || 'Значение не выбрано' }</span>
+        <div className={styles.combobox}>
+          <ComboBox
+            value={this.state.value}
+            onChange={({target: {value}}) => this.setState({value})}
+            renderValue={(value, info) => <span>{value}</span>}
+            source={getSource}
+            renderItem={foundItem}
+            placeholder="Выберите значение"
+            openButton
+          />
+        </div>
+      </div>
     </div>
-  </div>
-
-</div>
-
-export default App;
+  }
+}
